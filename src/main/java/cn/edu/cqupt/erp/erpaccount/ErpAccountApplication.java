1f2a7c5769afd2ec66a4397a8e280c4651159238
misa527528 <1353236065@qq.com>
2018-05-13 21:21:21 +0800

    #Other generate main architech

---
+++ src/main/java/cn/edu/cqupt/erp/erpaccount/ErpAccountApplication.java
@@ -0,0 +1,12 @@
+package cn.edu.cqupt.erp.erpaccount;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ErpAccountApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ErpAccountApplication.class, args);
+	}
+}
