600f6c07b4b11ac155406920b63a8e09e9aca6a6
Azaria_m <13882139946@163.com>
2018-05-20 23:01:28 +0800

    #Feature 修改了registerDao层接口

--- src/main/java/cn/edu/cqupt/erp/erpaccount/dao/RegisterDao.java
+++ src/main/java/cn/edu/cqupt/erp/erpaccount/dao/RegisterDao.java
@@ -5,15 +5,15 @@ import cn.edu.cqupt.erp.erpaccount.entity.Register;
 import java.util.List;
 
 public interface RegisterDao {
-    void addRegister(Register register);
+    int addRegister(Register register);
 
-    void updateRegister(Register register);
+    int  updateRegister(Register register);
 
     Register findByUserId(String registerId);
 
     List<Register> findAllRegister();
 
-    boolean deleteRegisterById(String registerId);
+    int deleteRegisterById(String registerId);
 
-    void updateApprovedUser(Register register);
+    int  updateApprovedUser(Register register);
 }
